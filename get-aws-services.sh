9b96fb5ca05fa51d17a25630ccd9f6e4c5dc063e
John Gakhokidze <66442890+virtiogroove@users.noreply.github.com>
2020-11-16 13:00:33 -0600

    Adding aws ssm part

--- get-aws-services.sh
+++ get-aws-services.sh
@@ -15,3 +15,18 @@ cat services.json |jq -r '.prices[]|.id as $d|([$d]|@csv)' |grep lustre
 #If you want to print with service full name - not elegant but works:
 
 cat services.json |jq -r '.prices[]|.id as $d|([.attributes|tostring ,$d]|@csv)'|awk -F ',' '{print $2, $4}'
+
+#AWS CLI
+#Get all regions
+aws ssm get-parameters-by-path — path /aws/service/global-infrastructure/regions — output json|jq .Parameters[].Name
+
+#Get all services:
+aws ssm get-parameters-by-path — path /aws/service/global-infrastructure/services — output json|jq .Parameters[].Name
+
+#Get services in specific region
+aws ssm get-parameters-by-path \
+  --path /aws/service/global-infrastructure/regions/us-east-1/services --output json | \
+  jq .Parameters[].Name
+  
+ #See also https://aws.amazon.com/blogs/aws/new-query-for-aws-regions-endpoints-and-more-using-aws-systems-manager-parameter-store/
+ 
